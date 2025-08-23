715e0239e4a1cdba2f445a45fb4f9d6a7aafa043
Felipe Sanfelice <felipesanfelice59@gmail.com>
2025-08-23 13:07:47 -0300

    Nova entrada do usuÃ¡rio

--- app/entrada-usuario/TesteExtracao.java
+++
@@ -1,7 +0,0 @@
-package org.example.util;
-
-public class TesteExtracao {
-    public static void main(String[] args) {
-        ExtratorJacoco.extrair("app/build/reports/jacoco/test/jacocoTestReport.xml");
-    }
-}
