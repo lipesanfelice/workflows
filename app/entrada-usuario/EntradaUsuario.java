092dce28ab7fe6f14ca3cd15a9b39944938d076a
Felipe Sanfelice <felipesanfelice59@gmail.com>
2025-09-02 19:49:14 -0300

    Nova entrada do usuÃ¡rio

--- app/entrada-usuario/EntradaUsuario.java
+++ app/entrada-usuario/EntradaUsuario.java
@@ -1,22 +1,17 @@
-import java.security.SecureRandom;
+import java.util.Random;
 
-public class GeradorSenha {
-    private static final String CARACTERES = 
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
-    
-    public static String gerarSenha(int tamanho) {
-        SecureRandom random = new SecureRandom();
-        StringBuilder senha = new StringBuilder();
-        
-        for (int i = 0; i < tamanho; i++) {
-            int index = random.nextInt(CARACTERES.length());
-            senha.append(CARACTERES.charAt(index));
-        }
-        
-        return senha.toString();
+public class GeradorCor {
+    public static String gerarCorRGB() {
+        Random random = new Random();
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return String.format("rgb(%d, %d, %d)", r, g, b);
     }
     
     public static void main(String[] args) {
-        System.out.println("Senha gerada: " + gerarSenha(12));
+        System.out.println("Cor gerada: " + gerarCorRGB());
+        System.out.println("Cor gerada: " + gerarCorRGB());
+        System.out.println("Cor gerada: " + gerarCorRGB());
     }
 }
