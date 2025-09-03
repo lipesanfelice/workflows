b29dc6e8f0974f9f7c0aebb1c3b9ed7272fa78d9
Felipe Sanfelice <felipesanfelice59@gmail.com>
2025-09-03 14:40:03 -0300

    Nova entrada do usuÃ¡rio

--- app/entrada-usuario/EntradaUsuario.java
+++ app/entrada-usuario/EntradaUsuario.java
@@ -1,19 +1,5 @@
-import java.util.Random;
-
-public class GeradorAleatorio {
+public class Exemplo {
     public static void main(String[] args) {
-        Random random = new Random();
-        
-        // Número inteiro entre 0 e 100
-        int numeroInteiro = random.nextInt(101);
-        System.out.println("Número inteiro: " + numeroInteiro);
-        
-        // Número decimal entre 0.0 e 1.0
-        double numeroDecimal = random.nextDouble();
-        System.out.println("Número decimal: " + numeroDecimal);
-        
-        // Boolean aleatório
-        boolean valorBooleano = random.nextBoolean();
-        System.out.println("Valor booleano: " + valorBooleano);
+        System.out.println("Hello World!");
     }
 }
