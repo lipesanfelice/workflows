6fe842b7b708f7f757771eb7291fbdc538e5096b
Felipe Sanfelice <felipesanfelice59@gmail.com>
2025-09-03 14:29:35 -0300

    Nova entrada do usuÃ¡rio

--- app/entrada-usuario/EntradaUsuario.java
+++ app/entrada-usuario/EntradaUsuario.java
@@ -1,17 +1,5 @@
-import java.util.Random;
-
-public class GeradorCor {
-    public static String gerarCorRGB() {
-        Random random = new Random();
-        int r = random.nextInt(256);
-        int g = random.nextInt(256);
-        int b = random.nextInt(256);
-        return String.format("rgb(%d, %d, %d)", r, g, b);
-    }
-    
+public class Exemplo {
     public static void main(String[] args) {
-        System.out.println("Cor gerada: " + gerarCorRGB());
-        System.out.println("Cor gerada: " + gerarCorRGB());
-        System.out.println("Cor gerada: " + gerarCorRGB());
+        System.out.println("Hello World!");
     }
 }
