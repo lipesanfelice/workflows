4d7813eaaa689f616db9779e53703feebfd1ad1f
Felipe Sanfelice <felipe@exemplo.com>
2025-10-01 09:23:22 -0300

    testes parte web

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,20 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.security.SecureRandom;
 
-public class SorteioNomes {
+public class GeradorSenha {
+    private static final String CARACTERES = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    
+    public static String gerarSenha(int tamanho) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder senha = new StringBuilder();
+        
+        for (int i = 0; i < tamanho; i++) {
+            int index = random.nextInt(CARACTERES.length());
+            senha.append(CARACTERES.charAt(index));
+        }
+        
+        return senha.toString();
+    }
+    
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>();
-        nomes.add("Ana");
-        nomes.add("Carlos");
-        nomes.add("Maria");
-        nomes.add("João");
-        nomes.add("Pedro");
-        nomes.add("Julia");
-        
-        Random random = new Random();
-        String nomeSorteado = nomes.get(random.nextInt(nomes.size()));
-        
-        System.out.println("Nome sorteado: " + nomeSorteado);
+        System.out.println("Senha gerada: " + gerarSenha(12));
     }
 }
