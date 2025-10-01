e29951eedb1bcac6a8482b3fd1114c25f78e1cbf
Felipe Sanfelice <felipe@exemplo.com>
2025-10-01 10:43:26 -0300

    testes parte web

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,23 +1,20 @@
-import java.util.Random;
-import java.util.Scanner;
-
-public class JogoDados {
+public class ConversorTemperatura {
+    
+    public static double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    
+    public static double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+    
+    public static double celsiusParaKelvin(double celsius) {
+        return celsius + 273.15;
+    }
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
-        System.out.println("Jogo de Dados - Quantos dados quer lançar?");
-        int quantidade = scanner.nextInt();
-        
-        int total = 0;
-        System.out.println("Resultados:");
-        for (int i = 0; i < quantidade; i++) {
-            int dado = random.nextInt(6) + 1;
-            System.out.println("Dado " + (i+1) + ": " + dado);
-            total += dado;
-        }
-        
-        System.out.println("Total: " + total);
-        scanner.close();
+        System.out.println("25°C para Fahrenheit: " + celsiusParaFahrenheit(25));
+        System.out.println("77°F para Celsius: " + fahrenheitParaCelsius(77));
+        System.out.println("0°C para Kelvin: " + celsiusParaKelvin(0));
     }
 }
