101084b051f29ec2b9774885f482e72d62a1a3a2
Felipe Sanfelice <felipe@exemplo.com>
2025-10-15 08:17:21 -0300

    testes parte web

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
-
-public class VerificadorParImpar {
+public class VerificadorIdade {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int idade = 20;
         
-        System.out.print("Digite um número: ");
-        int numero = scanner.nextInt();
+        System.out.println("Idade: " + idade + " anos");
         
-        if (numero % 2 == 0) {
-            System.out.println(numero + " é PAR");
+        if (idade < 0) {
+            System.out.println("Idade inválida!");
+        } else if (idade < 12) {
+            System.out.println("Criança");
+        } else if (idade < 18) {
+            System.out.println("Adolescente");
+        } else if (idade < 60) {
+            System.out.println("Adulto");
         } else {
-            System.out.println(numero + " é ÍMPAR");
+            System.out.println("Idoso");
         }
         
-        scanner.close();
+        System.out.println("Pode votar: " + (idade >= 16 ? "Sim" : "Não"));
+        System.out.println("Maior de idade: " + (idade >= 18 ? "Sim" : "Não"));
     }
 }
