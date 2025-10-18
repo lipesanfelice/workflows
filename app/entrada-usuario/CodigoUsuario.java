f3c83bb283655ff078f8d6360a2935934abedbea
Felipe Sanfelice <felipe@exemplo.com>
2025-10-18 09:11:08 -0300

    testes parte web

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,34 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-public class GerenciadorTarefas {
-    private List<String> tarefas = new ArrayList<>();
-    
-    public void adicionarTarefa(String tarefa) {
-        tarefas.add(tarefa);
-        System.out.println("Tarefa adicionada: " + tarefa);
-    }
-    
-    public void removerTarefa(int indice) {
-        if (indice >= 0 && indice < tarefas.size()) {
-            String removida = tarefas.remove(indice);
-            System.out.println("Tarefa removida: " + removida);
-        }
-    }
-    
-    public void listarTarefas() {
-        System.out.println("\n--- LISTA DE TAREFAS ---");
-        for (int i = 0; i < tarefas.size(); i++) {
-            System.out.println((i + 1) + ". " + tarefas.get(i));
-        }
-    }
-    
+public class VerificadorParImpar {
     public static void main(String[] args) {
-        GerenciadorTarefas gerenciador = new GerenciadorTarefas();
-        gerenciador.adicionarTarefa("Estudar Java");
-        gerenciador.adicionarTarefa("Fazer exercícios");
-        gerenciador.listarTarefas();
-        gerenciador.removerTarefa(0);
-        gerenciador.listarTarefas();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        
+        if (numero % 2 == 0) {
+            System.out.println(numero + " é PAR");
+        } else {
+            System.out.println(numero + " é ÍMPAR");
+        }
+        
+        scanner.close();
     }
 }
