d68cfc264078bf2c0945b2ad288432f13e71e42a
Felipe Sanfelice <felipe@exemplo.com>
2025-10-18 10:28:00 -0300

    testes parte web

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,50 +1,35 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Calculadora {
+public class JogoAdivinhacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         
-        System.out.println("=== CALCULADORA SIMPLES ===");
-        System.out.print("Digite o primeiro número: ");
-        double num1 = scanner.nextDouble();
+        int numeroSecreto = random.nextInt(100) + 1;
+        int tentativas = 0;
+        int palpite;
         
-        System.out.print("Digite o segundo número: ");
-        double num2 = scanner.nextDouble();
+        System.out.println("=== JOGO DE ADIVINHAÇÃO ===");
+        System.out.println("Estou pensando em um número entre 1 e 100.");
+        System.out.println("Tente adivinhar!");
         
-        System.out.println("\nEscolha a operação:");
-        System.out.println("1 - Soma (+)");
-        System.out.println("2 - Subtração (-)");
-        System.out.println("3 - Multiplicação (*)");
-        System.out.println("4 - Divisão (/)");
-        System.out.print("Opção: ");
-        
-        int opcao = scanner.nextInt();
-        double resultado = 0;
-        
-        switch (opcao) {
-            case 1:
-                resultado = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + resultado);
-                break;
-            case 2:
-                resultado = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + resultado);
-                break;
-            case 3:
-                resultado = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + resultado);
-                break;
-            case 4:
-                if (num2 != 0) {
-                    resultado = num1 / num2;
-                    System.out.println(num1 + " / " + num2 + " = " + resultado);
-                } else {
-                    System.out.println("Erro: Divisão por zero!");
-                }
-                break;
-            default:
-                System.out.println("Opção inválida!");
-        }
+        do {
+            System.out.print("\nDigite seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+            
+            if (palpite < numeroSecreto) {
+                System.out.println("Muito baixo! Tente um número maior.");
+            } else if (palpite > numeroSecreto) {
+                System.out.println("Muito alto! Tente um número menor.");
+            } else {
+                System.out.println("🎉 Parabéns! Você acertou!");
+                System.out.println("O número era: " + numeroSecreto);
+                System.out.println("Tentativas: " + tentativas);
+            }
+            
+        } while (palpite != numeroSecreto);
         
         scanner.close();
     }
