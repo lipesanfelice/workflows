d9d1052efcff804810d9d0f8fe6f5149ee4f70c8
Felipe Sanfelice <felipe@exemplo.com>
2025-10-23 11:33:49 -0300

    testes para cobertura

--- app/entrada-usuario/CodigoUsuario.java
+++ app/entrada-usuario/CodigoUsuario.java
@@ -1,20 +1,30 @@
-public class ConversorTemperatura {
+public class ValidadorCPF {
     
-    public static double celsiusParaFahrenheit(double celsius) {
-        return (celsius * 9/5) + 32;
+    public static boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+        
+        if (cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+        
+        int[] digitos = cpf.chars().map(Character::getNumericValue).toArray();
+        
+        int digito1 = calcularDigito(digitos, 9);
+        int digito2 = calcularDigito(digitos, 10);
+        
+        return digitos[9] == digito1 && digitos[10] == digito2;
     }
     
-    public static double fahrenheitParaCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5/9;
-    }
-    
-    public static double celsiusParaKelvin(double celsius) {
-        return celsius + 273.15;
+    private static int calcularDigito(int[] digitos, int posicao) {
+        int soma = 0;
+        for (int i = 0; i < posicao; i++) {
+            soma += digitos[i] * (posicao + 1 - i);
+        }
+        int resto = soma % 11;
+        return resto < 2 ? 0 : 11 - resto;
     }
     
     public static void main(String[] args) {
-        System.out.println("25°C para Fahrenheit: " + celsiusParaFahrenheit(25));
-        System.out.println("77°F para Celsius: " + fahrenheitParaCelsius(77));
-        System.out.println("0°C para Kelvin: " + celsiusParaKelvin(0));
+        System.out.println("CPF válido: " + validarCPF("123.456.789-09"));
     }
 }
